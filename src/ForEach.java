6f28bd6d245592e130cd54e02f43aaccea10ae2e
romuald11 <romuald117@wp.pl>
2022-11-23 19:35:40 +0100

    uzycie petli foreach

---
+++ src/ForEach.java
@@ -0,0 +1,7 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[]cats=new String[]{"Burek", "Garnfield", "Puszek", "Mruczek"};
+        for(String cat:cats) {
+            System.out.println(cat);
+        }
+}}
